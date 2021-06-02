db8efe2141150f80c0dbd9a8705f2afd92b2df62
gleam <nobody.roman@gmail.com>
2021-06-02 12:31:52 +0300

    feat: interns action

---
+++ interns-email-feedback.js
@@ -0,0 +1,45 @@
+const mg = require('mailgun-js');
+
+module.exports = {
+    name: "Intern Email Feedback",
+    description: "Sends 'hello' to registered intern",
+    key: "intern_email_feedback",
+    version: "0.0.6",
+    type: "action",
+    props: {
+        mailgun: {
+            type: "app",
+            app: "mailgun",
+        },
+        targetEmail: {
+            type: "string",
+            label: "Target email",
+        }
+    },
+    async run() {
+        const api_key = this.mailgun.$auth.api_key;
+        const domain = 'send.web-bee.ru';
+        const mailgun = mg({apiKey: api_key, domain: domain});
+        // return `hello world! mailgun.api_key`
+        const data = {
+            from: 'web-bee.ru <internship@web-bee.ru>',
+            to: this.targetEmail,
+            subject: 'Вы успешно записались на стажировку',
+            text: `Здравствуйте.
+
+Мы получили вашу заявку и в ближайшее время с вами свяжемся.
+Если вам необходимо что-то уточнить - можете ответить на это письмо, или написать в наш telegram-чат:
+https://t.me/joinchat/QRGwBxgK46xjZjFi
+
+Спасибо!`
+        };
+
+        return new Promise((resolve, reject) => {
+            mailgun.messages().send(data, function (error, body) {
+                if (error) return reject(error);
+                console.log(body);
+                resolve(body);
+            });
+        });
+    },
+}
